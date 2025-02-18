43648debe8b7f31da947863424df20a0fece44e7
Jim_Floyd <jamshid.elboev5111995@gmail.com>
2025-02-18 13:04:26 +0500

    1 2 tasks

--- query.sql
+++ query.sql
@@ -1,20 +1,13 @@
--- SELECT *
--- FROM sys.databases;
+select *
+from employees
 
--- CREATE DATABASE students;
--- CREATE table students_list
--- (
---     id INTEGER PRIMARY KEY,
---     first_name TEXT NOT NULL,
---     last_name TEXT NOT NULL,
---     emails VARCHAR(255) NOT NULL UNIQUE,
---     hire_date TEXT NOT NULL
--- );
+select *
+from employees
+where salary > (SELECT avg(salary)
+from employees)
 
--- INSERT INTO students_list (id, first_name, last_name, emails, hire_date) VALUES
--- (1, 'John', 'Doe', 'john.doe@example.com', '2021-09-01'),
--- (2, 'Jane', 'Smith', 'jane.smith@example.com', '2021-09-02'),
--- (3, 'Alice', 'Johnson', 'alice.johnson@example.com', '2021-09-03');
-
-SELECT *
-FROM students_list;
+select *
+from employees e
+where salary > (SELECT avg(salary)
+from employees
+where DepartmentID = e.DepartmentID)
